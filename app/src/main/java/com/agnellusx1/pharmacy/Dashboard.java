314a1a268db1fa67403d09490e82f613ec1f72c3
AgnellusX1 <43168803+AgnellusX1@users.noreply.github.com>
2019-12-31 09:30:16 +0530

    NEW: Search Bar

--- app/src/main/java/com/agnellusx1/pharmacy/Dashboard.java
+++ app/src/main/java/com/agnellusx1/pharmacy/Dashboard.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.SearchView;
 import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.agnellusx1.pharmacy.Adapters.NormalAdapter;
 import com.agnellusx1.pharmacy.Adapters.OrderSample;
@@ -19,13 +24,15 @@ import com.agnellusx1.pharmacy.Tabs.NormalTab;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class Dashboard extends AppCompatActivity {
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-    private TabAdapter tabAdapter;
-    FloatingActionButton AddBtn;
+   String BillNo;
+   String value="";
 
 
     @Override
@@ -33,6 +40,9 @@ public class Dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         FloatingActionButton AddBtn = findViewById(R.id.AddBtn);
+        ImageButton searchButton=findViewById(R.id.searchButton);
+        final EditText search=findViewById(R.id.search);
+
 
         AddBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,8 +52,42 @@ public class Dashboard extends AppCompatActivity {
                 finish();
             }
         });
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(Dashboard.this, "Searching", Toast.LENGTH_SHORT).show();
+                value=search.getText().toString();
+                DBconnect DB=new DBconnect();
+                Connection connection= DB.connectionclass();
+                if(connection==null){
+                    Toast.makeText(Dashboard.this, "Check Internet Connection", Toast.LENGTH_SHORT).show();
+                }else{
+                    String query="Select * from Vw_PharmacyDeliveries where PatientCode='"+value+"'";
+                    try {
+                        Statement stmt = connection.createStatement();
+                        ResultSet resultSet=stmt.executeQuery(query);
+                        if(resultSet.next()){
+                            Toast.makeText(Dashboard.this, "The !st query is good", Toast.LENGTH_SHORT).show();
+                            String query2 = "INSERT INTO Order_List (MIN,scanDate,PatientCode,PatientName,Status,Location)values('"+value+"',getDate(),'"+resultSet.getString("PatientCode")+"','"+resultSet.getString("PatientName")+"',1,'aggubaggu')";
+                            Statement stmt2=connection.createStatement();
+                            int z;
+                            z = stmt2.executeUpdate(query2);
+                            if (z>0){
+                                Toast.makeText(Dashboard.this, "...wuba luba dub dub", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(Dashboard.this, "puftttt", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
     }
-
-
-
 }
--- app/src/main/java/com/agnellusx1/pharmacy/DashboardList.java
+++ app/src/main/java/com/agnellusx1/pharmacy/DashboardList.java
@@ -10,6 +10,7 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.agnellusx1.pharmacy.Adapters.NormalAdapter;
