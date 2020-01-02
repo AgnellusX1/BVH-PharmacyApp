eff7f7293b2f31a22f8331a9df17443f135949f5
amrutsavadatti <amrutsavadatti@gmail.com>
2020-01-02 19:27:36 +0530

    "Hare Krishna" The Beginning

--- app/src/main/java/com/agnellusx1/pharmacy/MainActivity.java
+++ app/src/main/java/com/agnellusx1/pharmacy/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "'Hare Krishna'", Toast.LENGTH_SHORT).show();
 
         if (Dashboard.cacheCheck.equals("C")){
 
