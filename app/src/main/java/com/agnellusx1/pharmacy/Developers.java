5076cfb8ce70f42bed773914d97f937f1476e595
AgnellusX1 <43168803+AgnellusX1@users.noreply.github.com>
2020-01-11 19:34:13 +0530

    ADDED: Added About Developers

---
+++ app/src/main/java/com/agnellusx1/pharmacy/Developers.java
@@ -0,0 +1,14 @@
+package com.agnellusx1.pharmacy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Developers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developers);
+    }
+}
