2d438590b3190abef6a0ef99e7dbbf75c0fc06e5
AgnellusX1 <43168803+AgnellusX1@users.noreply.github.com>
2020-03-04 21:16:36 +0530

    Maintain: Updated Libraries and Readme

--- app/src/main/java/com/agnellusx1/pharmacy/Developers.java
+++ app/src/main/java/com/agnellusx1/pharmacy/Developers.java
@@ -18,13 +18,13 @@ public class Developers extends AppCompatActivity {
         TextView dev1=findViewById(R.id.Dev1);
         dev1.setClickable(true);
         dev1.setMovementMethod(LinkMovementMethod.getInstance());
-        String link1="<a href='https://www.linkedin.com/in/agnellus-fernandes-81232b192'>Agnellus Fernandes</a>";
+        String link1="<a href='https://www.linkedin.com/in/agnellus-fernandes-81232b192'> Agnellus Fernandes </a>";
         dev1.setText(Html.fromHtml(link1));
 
         TextView dev2=findViewById(R.id.Dev2);
         dev2.setClickable(true);
         dev2.setMovementMethod(LinkMovementMethod.getInstance());
-        String link2="<a href='http://linkedin.com/in/amrut-savadatti-277069183'>Amrut Savadatti</a>";
+        String link2="<a href='http://linkedin.com/in/amrut-savadatti-277069183'> Amrut Savadatti </a>";
         dev2.setText(Html.fromHtml(link2));
     }
 
